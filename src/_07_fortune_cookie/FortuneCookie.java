8e3e251fc70a49f6565effd82474e1bb87f2f99d
League Student <league@iMac-6.attlocal.net>
2019-09-21 17:26:38 -0700

    Body Part, Fortune Cookie, Rain Game

--- src/_07_fortune_cookie/FortuneCookie.java
+++ src/_07_fortune_cookie/FortuneCookie.java
@@ -9,25 +9,27 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 public class FortuneCookie implements ActionListener {
-	public void showButton() {
-		JFrame frame = new JFrame();
-		frame.setVisible(true);
-		JButton button = new JButton();
-		frame.add(button);
-		frame.pack();
-		button.addActionListener(this);
-        System.out.println("Button clicked");
-        
-        
-        
-	}
-        
-
+	 public void showButton() {
+         JFrame frame = new JFrame();
+         frame.setVisible(true);
+         JButton button = new JButton();
+         frame.add(button);
+         frame.pack();
+         button.addActionListener(this);
+         int random = new Random().nextInt(5);
+         if(random==0) {
+        	
+         }
+    }
+	 public static void main(String[] args) {
+         new FortuneCookie().showButton();
+         
+   }
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		JOptionPane.showMessageDialog(null, "woohoo");
 		
-	}	
+	}
 }
-
--- src/_07_fortune_cookie/FortuneCookieRunner.java
+++
@@ -1,7 +0,0 @@
-package _07_fortune_cookie;
-
-public class FortuneCookieRunner {
-    public static void main(String[] args) {
-        new FortuneCookie().showButton();
-  }
-}
